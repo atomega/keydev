5f80a9b14044b3a6546139df8fe04e8650b87ef9
atomega <kerem.yollu@gmail.com>
2020-07-13 13:44:42 +0100

    some scrip to proper setup a X11 env.

--- linux/firstsetup/modules/xsetup.sh
+++ linux/firstsetup/modules/xsetup.sh
@@ -7,3 +7,7 @@ sudo apt-get clean
 
 sudo apt-get install -y --no-install-recommends xserver-xorg
 sudo apt-get install -y --no-install-recommends xinit
+sudo apt-get install -y openbox lxterminal
+sudo apt-get install -y lightdm
+sudo apt-get install -y matchbox-keyboard
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
